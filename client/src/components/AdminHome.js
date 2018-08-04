b2d724a86ba7516c4b132e55d3e3e112d137387d
Alan Bischoff <alanlbischoff@gmail.com>
2018-08-03 21:45:30 -0600

    updated gallery

--- client/src/components/AdminHome.js
+++ client/src/components/AdminHome.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
-import { Header, Image, Segment, Input, Label, Divider } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { handleUpload, fetchPhotos } from '../reducers/photos';
+import { Segment, Image, Divider, Header, Grid, Loader, Dimmer, Icon, Label, Input } from 'semantic-ui-react';
+import Dropzone from 'react-dropzone';
 import styled from 'styled-components';
 
 
 class AdminHome extends Component {
+    state = { fileUploading: false };
+
+  componentDidMount() {
+    this.props.dispatch(fetchPhotos());
+  }
+
+  onDrop = (photos) => {
+    // dispatch the handleUpload action pass it the photo
+    this.toggleUploading();
+    this.props.dispatch(handleUpload(photos[0], this.toggleUploading));
+  }
+
+  toggleUploading = () => {
+    this.setState({ fileUploading: !this.state.fileUploading });
+  }
+
+  displayPhotos = () => {
+    // map through the state of photo and display them
+    return this.props.photos.map( photo => {
+      return(
+        <Grid.Column width={4} key={photo.id}>
+          <Image src={photo.url} fluid />
+        </Grid.Column>
+      );
+    });
+  }
 
     handleClick = (e) => {
         window.location = "/galleries"
@@ -15,17 +44,61 @@ class AdminHome extends Component {
         <div>
       <Segment basic textAlign='center'>
         <Header as='h1' textAlign='center'>HOME</Header>
+        <ImageWrapper>
         <Image src='https://adventuremissoula.com/wp-content/uploads/2014/02/Glacier-National-Park-Coeur-dAlene-rafting.jpg' width='350px' height='200px' onClick={this.handleClick}/>
+        </ImageWrapper>
+        { this.state.fileUploading ?
+          <Dimmer active>
+            <Loader>Loading</Loader>
+          </Dimmer> :
+          <Dropzone
+            onDrop={this.onDrop}
+            style={{border: 'none'}}
+            textalign='center'
+          >
+          <IconWrapper>
+          <Icon  name='plus' />
+            Add a Gallery
+          </IconWrapper>    
+          </Dropzone>
+        }
+        <Segment basic>
+          <Header as='h2'>The Clouds Photos</Header>
+          <Divider />
+          <Grid>
+            <Grid.Row>
+              { this.displayPhotos() }
+            </Grid.Row>
+          </Grid>
+        </Segment>
       </Segment>
       </div>
     );
   }
 }
 
+const IconWrapper = styled.div`
+display: flex;
+justify-content: center;
+width: 150px;
+padding: 10px;
+margin: 25px;
+border: 1px solid black;
+cursor: pointer;
+background-color: rgba(7,232,201,.3);
+`
+
+
 const ImageWrapper = styled.div`
 display: flex;
 justify-content: space-between;
-border: 2px solid black;
-`
+cursor: pointer;
 
-export default AdminHome;
+`
+const mapStateToProps = (state) => {
+  return { photos: state.photos };
+}
+
+export default connect(mapStateToProps)(AdminHome);
+
+
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,7 +12,6 @@ import FetchUser from './FetchUser';
 import Landing from './Landing';
 import Packages from './Packages';
 import AdminHome from './AdminHome';
-import Photos from './Photos';
 import Galleries from './Galleries'
 
 class App extends Component {
@@ -26,7 +25,6 @@ class App extends Component {
             <Route exact path='/' component={Landing} />
             <Route exact path='/home' component={Home} />
             <Route exect path='/packages' component={Packages} />
-            <Route exact path='/photos' component={Photos} />
             <Route exact path='/galleries' component={Galleries} />
             <ProtectedRoute exact path='/adminhome' component={AdminHome} />
             <AuthRoute exact path='/login' component={Login} />
